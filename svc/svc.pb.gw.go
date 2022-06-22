9c5ba798b27a08879c1ce6f44a3e288f22534d0d
amdf <787535+amdf@users.noreply.github.com>
2022-06-22 16:02:05 +0300

    add convert method to web gate

--- svc/svc.pb.gw.go
+++ svc/svc.pb.gw.go
@@ -111,13 +111,12 @@ func RegisterConvGetImageHandlerServer(ctx context.Context, mux *runtime.ServeMu
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/conv_get_img.ConvGetImage/Convert", runtime.WithHTTPPathPattern("/conv_get_img.ConvGetImage/Convert"))
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/conv_get_img.ConvGetImage/Convert", runtime.WithHTTPPathPattern("/v1/convert"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_ConvGetImage_Convert_0(ctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_ConvGetImage_Convert_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -135,13 +134,12 @@ func RegisterConvGetImageHandlerServer(ctx context.Context, mux *runtime.ServeMu
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/conv_get_img.ConvGetImage/Image", runtime.WithHTTPPathPattern("/v1/image"))
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/conv_get_img.ConvGetImage/Image", runtime.WithHTTPPathPattern("/v1/image"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_ConvGetImage_Image_0(ctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_ConvGetImage_Image_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -198,13 +196,12 @@ func RegisterConvGetImageHandlerClient(ctx context.Context, mux *runtime.ServeMu
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/conv_get_img.ConvGetImage/Convert", runtime.WithHTTPPathPattern("/conv_get_img.ConvGetImage/Convert"))
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/conv_get_img.ConvGetImage/Convert", runtime.WithHTTPPathPattern("/v1/convert"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_ConvGetImage_Convert_0(ctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_ConvGetImage_Convert_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -219,13 +216,12 @@ func RegisterConvGetImageHandlerClient(ctx context.Context, mux *runtime.ServeMu
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/conv_get_img.ConvGetImage/Image", runtime.WithHTTPPathPattern("/v1/image"))
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/conv_get_img.ConvGetImage/Image", runtime.WithHTTPPathPattern("/v1/image"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_ConvGetImage_Image_0(ctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_ConvGetImage_Image_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -240,7 +236,7 @@ func RegisterConvGetImageHandlerClient(ctx context.Context, mux *runtime.ServeMu
 }
 
 var (
-	pattern_ConvGetImage_Convert_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"conv_get_img.ConvGetImage", "Convert"}, ""))
+	pattern_ConvGetImage_Convert_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "convert"}, ""))
 
 	pattern_ConvGetImage_Image_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "image"}, ""))
 )
